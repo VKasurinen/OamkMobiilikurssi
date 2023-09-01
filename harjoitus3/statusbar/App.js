cc7c4e50959902ee583de840f2ff48964044efae
Väinö Kasurinen <vaino.kasurinen@hotmail.fi>
2023-09-01 15:04:48 +0300

    harjoitus 3

---
+++ harjoitus3/statusbar/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar 
+      style="auto" 
+      //backgroundColor='red'
+      hidden={true}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight: 0,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
