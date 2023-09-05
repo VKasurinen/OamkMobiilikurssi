e116457b55bcdf8b22eddaa1580049b6061ace07
Väinö Kasurinen <vaino.kasurinen@hotmail.fi>
2023-09-05 17:53:56 +0300

    harjoitus 7 melkein valmis

---
+++ harjoitus7/weatherapp/screens/Position.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import Weather from './Weather';
+
+const Position = () => {
+
+const [latitude, setLatitude] = useState(0);
+const [longitude, setLongitude] = useState(0);
+const [isLoading, setIsLoading] = useState(true);
+
+useEffect(() => {
+    (async() => {
+        let {status} = await Location.requestForegroundPermissionsAsync();
+        try {
+            if (status === 'granted'){
+                const location = await Location.getLastKnownPositionAsync({accuracy: 6});
+                setLatitude(location.coords.latitude);
+                setLongitude(location.coords.longitude);
+                setIsLoading(false);
+            } else {
+                setIsLoading(false)
+            }
+        } catch(error) {
+            alert(error);
+            setIsLoading(false);
+        }
+    })();
+}, [])
+
+  return (isLoading) ?  (
+    <View style={styles.container}><Text>Retrieving location...</Text></View>
+
+  ) : (
+    <View style={styles.container}>
+        <Text style={styles.label}>Your location</Text>
+        <Text>{latitude.toFixed(3)},{longitude.toFixed(3)}</Text>
+        <Weather latitude={latitude} longitude={longitude} />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    label:{
+        fontWeight: "bold",
+        marginTop: 10,
+    },
+  });
+
+
+export default Position
---
+++ harjoitus7/weatherapp/screens/Weather.js
@@ -0,0 +1,59 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'api.openweathermap.org/data/2.5/weather?';
+const API_KEY = 'fd6ee9aa697124b327994d93e2014283';
+const ICON_URL = 'http://openweathermap.org/img/wn/';
+
+const Weather = ({latitude, longitude}) => {
+
+const [temp, setTemp] = useState(0);
+const [description, setDescription] = useState("");
+const [icon, setIcon] = useState("");
+
+useEffect(() => {
+    const url = API_URL + 
+    'lat=' + latitude +
+    'lon=' + longitude + 
+    '&units=metric' +
+    '&appid=' + API_KEY;
+    fetch(url)
+    .then(res => res.json())
+    .then(
+        (result) => {
+            setTemp(result.main.temp);
+            setDescription(result.weather[0].description);
+            // setIcon(ICON_URL + result.weather[0].icon + '@2x.png');
+        }, (error) => {
+            alert(error);
+            console.log(error);
+        }
+    )
+}, [])
+
+  return (
+    <>
+    <Text style={styles.label}>Temperature</Text>
+    <Text>{temp}</Text>
+    <Text style={styles.label}>Description</Text>
+    <Text>{description}</Text>
+    {/* <Image source={{uri: icon}} style={{width: 100, height: 100}} /> */}
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    label: {
+        fontWeight: 'bold',
+        marginTop: 10,
+    }
+  });
+
+export default Weather
