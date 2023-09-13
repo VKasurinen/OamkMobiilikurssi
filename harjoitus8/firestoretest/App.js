d13393071d0bdb53b8882bbca656d91fc5d53bc6
Väinö Kasurinen <vaino.kasurinen@hotmail.fi>
2023-09-13 14:40:10 +0300

    tehtävä 8 valmis

--- harjoitus8/firestoretest/App.js
+++ harjoitus8/firestoretest/App.js
@@ -1,13 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { firestore } from './firebase/Config';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { addDoc, collection, firestore, MESSAGES, serverTimestamp, onSnapshot, query, orderBy } from './firebase/Config';
+import { useEffect, useState } from 'react';
+import { convertFirebaseTimeStampToJS } from './helpers/Functions';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    const q = query(collection(firestore, MESSAGES),orderBy("created", "desc")); 
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const tempMessages = [];
+
+      querySnapshot.forEach((doc) => {
+
+        const messageObject = {
+          id: doc.id,
+          text: doc.data().text,
+          created: convertFirebaseTimeStampToJS(doc.data().created),
+        };
+        tempMessages.push(messageObject);
+      });
+      setMessages(tempMessages);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+
+  const save = async() => {
+    const docRef = await addDoc(collection(firestore, MESSAGES), {
+      text: newMessage,
+      created: serverTimestamp()
+    }).catch (error => console.log(error))
+
+    setNewMessage('')
+    console.log('Message saved.')
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {
+          messages.map((message) =>(
+            <View style={styles.message} key={message.id}>
+              <Text style={styles.messageInfo}>{message.created}</Text>
+              <Text>{message.text}</Text>
+            </View>
+          ))
+        }
+        
+      </ScrollView>
+      <TextInput placeholder="Send message..." value={newMessage} onChangeText={text => setNewMessage(text)}/>
+      <Button title="Send" type="button" onPress={save}/>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +67,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
+
+  message: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#f5f5f5",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  messageInfo: {
+    fontSize: 12
+  }
 });
--- harjoitus8/firestoretest/firebase/Config.js
+++ harjoitus8/firestoretest/firebase/Config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import {getFirestore, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy} from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +15,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const firestore = getFirestore();
-export {firestore};
+
+const MESSAGES = 'messages'
+
+export {firestore, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy, MESSAGES};
